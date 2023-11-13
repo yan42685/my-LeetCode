63f01c8c44f36d5d6a3691d8f251cbd0c11352ed
alex <419725794@qq.com>
2023-11-13 19:36:10 +0800

    Autocommit 11/13/2023 19:36:10

--- editor/cn/[4]寻找两个正序数组的中位数.java
+++ editor/cn/[4]寻找两个正序数组的中位数.java
@@ -1,106 +1,80 @@
 //leetcode submit region begin(Prohibit modification and deletion)
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int total = nums1.length + nums2.length;
+        if (total % 2 == 1) {
+            return findKthElement(nums1, nums2, total / 2 + 1);
+        } else {
+            return (findKthElement(nums1, nums2, total / 2) + findKthElement(nums1, nums2, total / 2 + 1)) / 2.0;
+        }
+    }
+
+    public int findKthElement(int[] nums1, int[] nums2, int k) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int left = Math.max(0, k - n);
+        int right = Math.min(k - 1, m - 1);
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (k - mid - 1 < n && nums1[mid] > nums2[k - mid - 1]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        int nums1LeftMax = left == 0 ? Integer.MIN_VALUE : nums1[left - 1];
+        int nums2LeftMax = left == k ? Integer.MIN_VALUE : nums2[k - left - 1];
+        return Math.max(nums1LeftMax, nums2LeftMax);
+    }
+
+}
+
+// 下面有带注释版
 //public class Solution {
 //    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
 //        int total = nums1.length + nums2.length;
-//        // 是否偶数
-//        if (total % 2 == 0) {
-//            return (findKth(nums1, nums2, total / 2) + findKth(nums1, nums2, total / 2 + 1)) / 2.0;
-//        } else {
+//        // 是否奇数
+//        if (total % 2 == 1) {
 //            return findKth(nums1, nums2, total / 2 + 1);
+//        } else {
+//            return (findKth(nums1, nums2, total / 2) + findKth(nums1, nums2, total / 2 + 1)) / 2.0;
 //        }
 //    }
 //
 //    private int findKth(int[] nums1, int[] nums2, int k) {
-//        int left1 = 0;
-//        int right1 = nums1.length - 1;
-//        int left2 = 0;
-//        int right2 = nums2.length - 1;
+//        int m = nums1.length;
+//        int n = nums2.length;
+//        int index1 = 0;
+//        int index2 = 0;
+//        // 不断排除小于kth的数，并减少k
 //        while (true) {
-//            // 闭区间为空
-//            if (left1 > right1) {
-//                return nums2[left2 + k - 1];
+//            // 已知三个边界条件
+//            if (index1 == m) {
+//                return nums2[index2 + k - 1];
 //            }
-//            if (left2 > right2) {
-//                return nums1[left1 + k - 1];
+//            if (index2 == n) {
+//                return nums1[index1 + k - 1];
 //            }
+//            // 这个判断要放在最后因为需要前两个判断排除数组越界情况
 //            if (k == 1) {
-//                return Math.min(nums1[left1], nums2[left2]);
+//                return Math.min(nums1[index1], nums2[index2]);
 //            }
-//            int mid1 = left1 + (right1 - left1) / 2;
-//            int mid2 = left2 + (right2 - left2) / 2;
-//            if (nums1[mid1] <= nums2[mid2]) {
-//                int excludeCount = mid1 - left1 + 1;
-//                if (excludeCount == k) {
-//                    return nums1[mid1];
-//                } else if (excludeCount < k) {
-//                    left1 = mid1 + 1;
-//                    k -= excludeCount;
-//                } else {
-//                    right2 = mid2 - 1;
-//                    --k;
-//                }
-//            } else if (nums1[mid1] > nums2[mid2]) {
-//                int excludeCount = mid2 - left2 + 1;
-//                if (excludeCount == k) {
-//                    return nums2[mid2];
-//                } else if (excludeCount < k) {
-//                    left2 = mid2 + 1;
-//                    k -= excludeCount;
-//                } else {
-//                    right1 = mid1-1;
-//                }
+//            // 每次尽量排除k/2个数 [index, p]之间的数
+//            // 即将被排除的最大的数
+//            int p1 = Math.min(index1 + k / 2 - 1, m - 1);
+//            int p2 = Math.min(index2 + k / 2 - 1, n - 1);
+//            if (nums1[p1] <= nums2[p2]) {
+//                k -= p1 - index1 + 1;
+//                index1 = p1 + 1;
+//            } else {
+//                k -= p2 - index2 + 1;
+//                index2 = p2 + 1;
 //            }
 //        }
-//
 //    }
-//
 //}
-
-
-// 下面有带注释版
-public class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int total = nums1.length + nums2.length;
-        // 是否奇数
-        if (total % 2 == 1) {
-            return findKth(nums1, nums2, total / 2 + 1);
-        } else {
-            return (findKth(nums1, nums2, total / 2) + findKth(nums1, nums2, total / 2 + 1)) / 2.0;
-        }
-    }
-
-    private int findKth(int[] nums1, int[] nums2, int k) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int index1 = 0;
-        int index2 = 0;
-        // 不断排除小于kth的数，并减少k
-        while (true) {
-            // 已知三个边界条件
-            if (index1 == m) {
-                return nums2[index2 + k - 1];
-            }
-            if (index2 == n) {
-                return nums1[index1 + k - 1];
-            }
-            // 这个判断要放在最后因为需要前两个判断排除数组越界情况
-            if (k == 1) {
-                return Math.min(nums1[index1], nums2[index2]);
-            }
-            // 每次尽量排除k/2个数 [index, p]之间的数
-            // 即将被排除的最大的数
-            int p1 = Math.min(index1 + k / 2 - 1, m - 1);
-            int p2 = Math.min(index2 + k / 2 - 1, n - 1);
-            if (nums1[p1] <= nums2[p2]) {
-                k -= p1 - index1 + 1;
-                index1 = p1 + 1;
-            } else {
-                k -= p2 - index2 + 1;
-                index2 = p2 + 1;
-            }
-        }
-    }
-}
-
-
+//
+//
 //leetcode submit region end(Prohibit modification and deletion)
