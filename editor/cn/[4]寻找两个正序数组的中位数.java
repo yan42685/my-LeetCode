84463966d61cb770347629354a29c715d7b0cd5c
alex <419725794@qq.com>
2023-11-13 21:36:12 +0800

    Autocommit 11/13/2023 21:36:11

--- editor/cn/[4]寻找两个正序数组的中位数.java
+++ editor/cn/[4]寻找两个正序数组的中位数.java
@@ -1,4 +1,5 @@
 //leetcode submit region begin(Prohibit modification and deletion)
+
 /**
  * 建议跳过此题，看懂题解也写不出来的
  */
@@ -14,27 +15,26 @@ public class Solution {
     }
 
     private int findKth(int[] nums1, int[] nums2, int k) {
-        int right1 = Math.min(k - 1, nums1.length - 1);
-        int right2 = Math.min(k - 1, nums2.length - 1);
+        int right1 = nums1.length;
+        int right2 = nums2.length;
         int left1 = 0;
         int left2 = 0;
         // 不断排除小于kth的数，并减少k
         while (true) {
             // 已知三个边界条件
-            if (left1 == right1 + 1) {
+            if (left1 == right1) {
                 return nums2[left2 + k - 1];
             }
-            if (left2 == right2 + 1) {
+            if (left2 == right2) {
                 return nums1[left1 + k - 1];
             }
             // 这个判断要放在最后因为需要前两个判断排除数组越界情况
             if (k == 1) {
                 return Math.min(nums1[left1], nums2[left2]);
             }
-            // 每次尽量排除k/2个数 [index, p]之间的数
-            // 即将被排除的最大的数
-            int p1 = Math.min(left1 + k / 2 - 1, right1);
-            int p2 = Math.min(left2 + k / 2 - 1, right2);
+            // 排除k个元素
+            int p1 = Math.min(left1 + k / 2 - 1, right1 - 1);
+            int p2 = Math.min(left2 + k / 2 - 1, right2 - 1);
             if (nums1[p1] <= nums2[p2]) {
                 k -= p1 - left1 + 1;
                 left1 = p1 + 1;
