8a363e9242fe85ab988224089bdb1214480a77c5
alex <419725794@qq.com>
2023-11-14 22:36:10 +0800

    Autocommit 11/14/2023 22:36:10

--- editor/cn/[378]有序矩阵中第 K 小的元素.java
+++ editor/cn/[378]有序矩阵中第 K 小的元素.java
@@ -3,29 +3,37 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
-        int x = (int)Math.sqrt(k - 1);
-        x = Math.min(x, n - 1); // 防止 x 超出界限
+        int left = matrix[0][0], right = matrix[n - 1][n - 1];
 
-        int row = x, col = 0; // 第 x+1 行和第 x+1 列的起始位置
-        int count = x * x; // 排除的元素数量
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            int count = countNotGreater(matrix, mid);
 
-        while (count < k - 1) {
-            if (col == n - 1 || (row > 0 && matrix[row - 1][x] < matrix[x][col])) {
-                // 移动行指针
-                row--;
+            if (count < k) {
+                left = mid + 1;
             } else {
-                // 移动列指针
-                col++;
+                right = mid;
             }
-            count++;
         }
 
-        // 判断应该从行还是列中取元素
-        if (row == 0 || (col < n && matrix[x][col] < matrix[row - 1][x])) {
-            return matrix[x][col];
-        } else {
-            return matrix[row - 1][x];
+        return left;
+    }
+
+    private int countNotGreater(int[][] matrix, int target) {
+        int n = matrix.length;
+        int i = n - 1, j = 0;
+        int count = 0;
+
+        while (i >= 0 && j < n) {
+            if (matrix[i][j] > target) {
+                i--;
+            } else {
+                count += i + 1;
+                j++;
+            }
         }
+
+        return count;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
