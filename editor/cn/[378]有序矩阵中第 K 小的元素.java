76cbc2720758d9eb57385a9dadbb81bf291a951e
alex <419725794@qq.com>
2023-11-15 08:36:10 +0800

    Autocommit 11/15/2023 08:36:10

--- editor/cn/[378]有序矩阵中第 K 小的元素.java
+++ editor/cn/[378]有序矩阵中第 K 小的元素.java
@@ -1,38 +1,43 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * 解空间是[min, max]之间的整数集合，min是左上角，max是右下角
+ * 时间复杂度O(Nlog(max-min))  空间O(1)
+ * 相对于第4题，只有一种解空间看法是可以写出代码的
+ * 如果矩阵的数据换成浮点数则不能用这个方法
+ */
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
         int n = matrix.length;
-        int left = matrix[0][0], right = matrix[n - 1][n - 1];
+        int left = matrix[0][0];
+        int right = matrix[n - 1][n - 1];
 
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2;
-            int count = countNotGreater(matrix, mid);
-
-            if (count < k) {
+            if (lessOrEqualCount(matrix, mid) <= k - 1) {
                 left = mid + 1;
             } else {
-                right = mid;
+                right = mid - 1;
             }
         }
 
         return left;
     }
 
-    private int countNotGreater(int[][] matrix, int target) {
+    private int lessOrEqualCount(int[][] matrix, int target) {
         int n = matrix.length;
-        int i = n - 1, j = 0;
+        int i = 0;
+        int j = n - 1;
         int count = 0;
-
-        while (i >= 0 && j < n) {
-            if (matrix[i][j] > target) {
-                i--;
+        while (i <= n - 1 && j >= 0) {
+            if (matrix[i][j] <= target) {
+                count += j + 1;
+                ++i;
             } else {
-                count += i + 1;
-                j++;
+                --j;
             }
         }
-
         return count;
     }
 }
