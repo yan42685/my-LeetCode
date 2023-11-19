098a61a152826c44a9c0adca682650792110b31a
alex <419725794@qq.com>
2023-11-19 13:36:11 +0800

    Autocommit 11/19/2023 13:36:11

--- editor/cn/[1575]统计所有可行路径.java
+++ editor/cn/[1575]统计所有可行路径.java
@@ -2,20 +2,24 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int countRoutes(int[] locations, int start, int finish, int fuel) {
-
+        long result = dfs(locations, start, finish, fuel);
+        return (int) (result % (1e9 + 7));
     }
 
     private long dfs(int[] locations, int start, int finish, int fuel) {
-        if (fuel < Math.abs(locations[start] - locations[finish])) {
+        int consumption = Math.abs(locations[start] - locations[finish]);
+        if (fuel < consumption) {
             return 0L;
         }
         long result = 1;
-        for (int location : locations) {
-            if (location != finish) {
-                result += dfs(locations, location, finish, fuel - Math.abs(location - finish));
+        for (int i = 0; i < locations.length; i++) {
+            if (i != start) {
+                
+                result += dfs(locations, start, i, fuel) +
+                        dfs(locations, i, finish, fuel - Math.abs(locations[start] - locations[i]));
             }
         }
-        return reuslt;
+        return result;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
