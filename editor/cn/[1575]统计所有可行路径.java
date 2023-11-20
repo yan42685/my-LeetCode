04b7fe5a2a8dd0a8d883baf8786d610ca92151e1
alex <419725794@qq.com>
2023-11-20 16:36:12 +0800

    Autocommit 11/20/2023 16:36:11

--- editor/cn/[1575]统计所有可行路径.java
+++ editor/cn/[1575]统计所有可行路径.java
@@ -1,6 +1,5 @@
-
-//leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    private static final int MOD = (int) (1e9 + 7);
     int[][] memo;
 
     public int countRoutes(int[] locations, int start, int finish, int fuel) {
@@ -15,50 +14,21 @@ class Solution {
         if (fuel < 0) {
             return 0;
         }
+
         if (memo[start][fuel] != -1) {
             return memo[start][fuel];
         }
+
         int count = start == finish ? 1 : 0;
+
         for (int i = 0; i < locations.length; i++) {
             if (i != start) {
                 int consumption = Math.abs(locations[start] - locations[i]);
-                count = (count + dfs(locations, i, finish, fuel - consumption)) % ((int) 1e9 + 7);
+                count = (count + dfs(locations, i, finish, fuel - consumption)) % MOD;
             }
         }
         memo[start][fuel] = count;
         return count;
     }
 }
-
-//public class Solution {
-//    private static final int MOD = (int) 1e9 + 7;
-//    // 缓存dfs结果
-//    private int[][] memo;
-//
-//    public int countRoutes(int[] locations, int start, int finish, int fuel) {
-//        memo = new int[locations.length][fuel + 1];
-//        for (int[] row : memo) {
-//            Arrays.fill(row, -1);
-//        }
-//        return dfs(locations, start, finish, fuel);
-//    }
-//
-//    private int dfs(int[] locations, int current, int finish, int fuel) {
-//        if (fuel < 0) {
-//            return 0;
-//        }
-//        if (memo[current][fuel] != -1) {
-//            return memo[current][fuel];
-//        }
-//        // 不消耗燃料到终点的路径数
-//        int count = current == finish ? 1 : 0;
-//        for (int i = 0; i < locations.length; i++) {
-//            if (i != current) {
-//                count = (count + dfs(locations, i, finish, fuel - Math.abs(locations[current] - locations[i]))) % MOD;
-//            }
-//        }
-//        memo[current][fuel] = count;
-//        return count;
-//    }
-//}
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/cn/[576]出界的路径数.java
@@ -0,0 +1,8 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
