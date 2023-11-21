29fbd6396f681365bb6db51e3c62164396ebec9c
alex <419725794@qq.com>
2023-11-21 21:36:11 +0800

    Autocommit 11/21/2023 21:36:11

--- editor/cn/[1301]最大得分的路径数目.java
+++ editor/cn/[1301]最大得分的路径数目.java
@@ -8,43 +8,74 @@ class Solution {
     public int[] pathsWithMaxScore(List<String> board) {
         this.board = board;
         int n = board.size();
-        if (n == 2) {
-            return new int[]{0, 0};
-        }
+//        if (n == 2) {
+//            return new int[]{0, 0};
+//        }
         int[][] dp = new int[n][n];
-        int[][] count=  new int[n][n];
+        int[][] count = new int[n][n];
         dp[n - 1][n - 1] = 0;
+        count[n - 1][n - 1] = 1;
         for (int i = n - 2; i >= 0 && getNum(i, n - 1) != OBSTACLE; i--) {
             dp[i][n - 1] = (dp[i + 1][n - 1] + getNum(i, n - 1)) % MOD;
+            count[i][n - 1] = 1;
         }
         for (int j = n - 2; j >= 0 && getNum(n - 1, j) != OBSTACLE; j--) {
             dp[n - 1][j] = (dp[n - 1][j + 1] + getNum(n - 1, j)) % MOD;
+            count[n - 1][j] = 1;
         }
         for (int i = n - 2; i >= 0; i--) {
             for (int j = n - 2; j >= 0; j--) {
-                if (getNum(i, j) != OBSTACLE && !(i == 0 && j == 0)) {
-                    int max = Math.max(Math.max(dp[i + 1][j], dp[i][j + 1]), dp[i + 1][j + 1]);
-                    dp[i][j] = max == OBSTACLE ? OBSTACLE : (max + getNum(i, j)) % MOD;
+                if (getNum(i, j) == OBSTACLE) {
+                    dp[i][j] = OBSTACLE;
+                    count[i][j] = OBSTACLE;
+                } else {
+                    int[] res = getMaxAndCount(new int[]{dp[i + 1][j], count[i + 1][j]},
+                            new int[]{dp[i][j + 1], count[i][j + 1]},
+                            new int[]{dp[i + 1][j + 1], count[i + 1][j + 1]});
+                    if (res[0] == OBSTACLE) {
+                        dp[i][j] = OBSTACLE;
+                        count[i][j] = OBSTACLE;
+                    } else {
+                        dp[i][j] = (res[0] + getNum(i, j)) % MOD;
+                        count[i][j] = res[1];
+                    }
                 }
             }
         }
-        int max = Math.max(dp[0][1], dp[1][0]);
-        if (max == 0 && dp[1][1] == OBSTACLE) {
-            return new int[]{0, 0};
-        } else {
-            int count = dp[0][1] == dp[1][0] ? 2 : 1;
-            return new int[]{max, count};
-        }
+        return new int[]{dp[0][0], count[0][0]};
     }
 
     private int getNum(int row, int col) {
         char ch = board.get(row).charAt(col);
         if (ch == 'X') {
             return OBSTACLE;
+        } else if (ch == 'E') {
+            return 0;
         } else {
             return ch - '0';
         }
     }
+
+    /**
+     * 获取最大的score和总的count
+     */
+    private int[] getMaxAndCount(int[]... dps) {
+        int max = OBSTACLE;
+        int count = OBSTACLE;
+        for (int[] dp : dps) {
+            if (dp[0] > max) {
+                // 初次遇到非OBSTACLE的点
+                if (max == OBSTACLE) {
+                    count = dp[1];
+                }
+                max = dp[0];
+            } else if (dp[0] == max && max != OBSTACLE) {
+                count = (count + dp[1]) % MOD;
+            }
+        }
+        return new int[]{max, count};
+    }
 }
+
 
 //leetcode submit region end(Prohibit modification and deletion)
