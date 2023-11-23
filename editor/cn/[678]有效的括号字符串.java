8f3d4f6dd5e7b30b8b8a32cdc193bf9868cc304f
alex <419725794@qq.com>
2023-11-23 19:36:11 +0800

    Autocommit 11/23/2023 19:36:11

--- editor/cn/[678]有效的括号字符串.java
+++ editor/cn/[678]有效的括号字符串.java
@@ -2,7 +2,22 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean checkValidString(String s) {
-
+        int left = 0;
+        int auto = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == '(') {
+                ++left;
+            } else if (ch == ')') {
+                --left;
+                if (left + auto < 0) {
+                    return false;
+                }
+            } else if (ch == '*') {
+                ++auto;
+            }
+        }
+        return left + auto >= 0 && left <= auto ? true : false;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
