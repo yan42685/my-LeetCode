b2cadcbf8bced9b00a5df46d0f5e76d649229844
alex <419725794@qq.com>
2023-11-24 08:36:11 +0800

    Autocommit 11/24/2023 08:36:11

--- editor/cn/[LCR 126]斐波那契数.java
+++ editor/cn/[LCR 126]斐波那契数.java
@@ -1,15 +1,35 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+    private static final int MOD = (int) 1e9 + 7;
+
     public int fib(int n) {
         if (n <= 1) {
             return n;
         }
-        int[] dp = {0, 1};
+        int a = 0;
+        int b = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i % 2] = dp[(i - 1) % 2] + dp[(i - 2) % 2];
+            int c = (a + b) % MOD;
+            a = b;
+            b = c;
         }
-        return dp[n % 2];
+        return b;
     }
 }
+
+//class Solution {
+//    private static final int MOD = (int) 1e9 + 7;
+//
+//    public int fib(int n) {
+//        if (n <= 1) {
+//            return n;
+//        }
+//        int[] dp = {0, 1};
+//        for (int i = 2; i <= n; i++) {
+//            dp[i % 2] = (dp[(i - 1) % 2] + dp[(i - 2) % 2]) % MOD;
+//        }
+//        return dp[n % 2];
+//    }
+//}
 //leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/cn/[LCR 161]连续天数的最高销售额.java
@@ -0,0 +1,14 @@
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int maxSales(int[] sales) {
+        int result = sales[0];
+        int tmpMax = sales[0];
+        for (int i = 1; i < sales.length; i++) {
+            tmpMax = Math.max(tmpMax + sales[i], tmpMax);
+            result = Math.max(result, tmpMax);
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
