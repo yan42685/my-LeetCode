a3f41907612735bd4af18b5c9f68297a389ce76f
alex <419725794@qq.com>
2023-11-25 14:36:11 +0800

    Autocommit 11/25/2023 14:36:11

--- editor/cn/[629]K 个逆序对数组.java
+++ editor/cn/[629]K 个逆序对数组.java
@@ -8,59 +8,30 @@
  * 所以dp[i][j] = sum(dp[i-1][j-p]), p ∈ [0, min(j, i-1)]
  * 2. 由于存在连续项求和，所以用前缀数组优化
  */
-//class Solution {
-//    private static final int MOD = (int) 1e9 + 7;
-//
-//    public int kInversePairs(int n, int k) {
-//        if (k > (n * (n - 1)) / 2) {
-//            return 0;
-//        }
-//        int[][] dp = new int[n + 1][k + 1];
-//        for (int i = 1; i <= n; i++) {
-//            for (int j = 0; j <= k; j++) {
-//                if (j == 0) {
-//                    dp[i][j] = 1;
-//                    continue;
-//                }
-//                for (int p = 0; p <= Math.min(j, i - 1); p++) {
-//                    dp[i][j] = (dp[i][j] + dp[i - 1][j - p]) % MOD;
-//                }
-//            }
-//        }
-//        return dp[n][k];
-//    }
-//}
-
 class Solution {
     private static final int MOD = (int) 1e9 + 7;
 
     public int kInversePairs(int n, int k) {
-        if (k > (n * (n - 1)) / 2) {
-            return 0;
-        }
-
         int[][] dp = new int[n + 1][k + 1];
-        int[] sum = new int[k + 1];  // 前缀和数组在循环外部创建
-        dp[0][0] = 1;
-
+        int[][] preSum = new int[n + 1][k + 1];
         for (int i = 1; i <= n; i++) {
-            sum[0] = dp[i - 1][0];
-
-            for (int j = 1; j <= k; j++) {
-                sum[j] = (sum[j - 1] + dp[i - 1][j]) % MOD;
-            }
-
             for (int j = 0; j <= k; j++) {
-                dp[i][j] = sum[j];
-                if (j >= i) {
-                    dp[i][j] = (dp[i][j] - sum[j - i] + MOD) % MOD;
+                if (j == 0) {
+                    dp[i][j] = 1;
+                    preSum[i][j] = 1;
+                    continue;
+                }
+                for (int p = 0; p <= Math.min(j, i - 1); p++) {
+                    dp[i][j] = (dp[i][j] + dp[i - 1][j - p]) % MOD;
                 }
             }
         }
-
         return dp[n][k];
     }
 }
+
+
+//
 //class Solution {
 //    int mod = (int) 1e9 + 7;
 //
@@ -79,26 +50,4 @@ class Solution {
 //    }
 //}
 //
-//public class Solution {
-//
-//    private static final int MOD = 1_000_000_007;
-//
-//    public int kInversePairs(int n, int k) {
-//        int[][] dp = new int[n + 1][k + 1];
-//
-//        for (int i = 0; i <= n; i++) {
-//            dp[i][0] = 1;
-//        }
-//
-//        for (int i = 1; i <= n; i++) {
-//            for (int j = 1; j <= k; j++) {
-//                for (int p = 0; p <= Math.min(j, i - 1); p++) {
-//                    dp[i][j] = (dp[i][j] + dp[i - 1][j - p]) % MOD;
-//                }
-//            }
-//        }
-//
-//        return dp[n][k];
-//    }
-//}
 //leetcode submit region end(Prohibit modification and deletion)
