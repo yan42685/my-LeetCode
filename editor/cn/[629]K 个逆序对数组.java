1f7a4ae4ca9e77a609673d0621bbe38bcab0a5e6
alex <419725794@qq.com>
2023-11-25 19:36:11 +0800

    Autocommit 11/25/2023 19:36:11

--- editor/cn/[629]K 个逆序对数组.java
+++ editor/cn/[629]K 个逆序对数组.java
@@ -2,64 +2,77 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 
 /**
+ * 状态转移优化版，时间复杂度O(N * K) 空间复杂度O(N)
  * https://leetcode.cn/problems/k-inverse-pairs-array/solutions/1096189/tong-ge-lai-shua-ti-la-yi-ti-wu-jie-bao-ej4ym/
+ * dp[i][j] i个数能形成k个逆序对的排列数
+ * 第i个数最多增加i-1个逆序对，取决于i所在的位置
  * dp[i][j] = dp[i-1][j] + dp[i-1][j-1] + ... + dp[i-1][j-(i-1)]
  * dp[i][j-1] =            dp[i-1][j-1] + ... + dp[i-1][j-1-(i-2)] + dp[i-1][j-1-(i-1)]
  * 上减下、移项，得到
- * dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-i]
+ * dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-i] （如果j < i, 则为dp[i-1][j-i] == 0)
  */
+
 class Solution {
-    int mod = 1000000007;
+    private static final int MOD = (int) 1e9 + 7;
 
     public int kInversePairs(int n, int k) {
-        if (k > maxPairs(n)) {
-            return 0;
-        }
-        //dp[i][j]表示到n为止，逆序数为k的个数
-        int dp[][] = new int[n + 1][k + 1];
-        for (int i = 1; i <= n; i++) {
-            //0个逆序数对只有一种情况
-            dp[i][0] = 1;
-            for (int j = 1; j <= Math.min(k, maxPairs(i)); j++) {
-                //从前面可以得到dp[i][j] = sum(dp[i-1][m])，m >= Math.max(0, j - i + 1)
-                //这里是歪打正着，那就来分析一下为什么会这样
-                //dp[i][j] = dp[i - 1][j - i + 1] + dp[i - 1][j - i + 2] + ... + dp[i - 1][j];
-                //dp[i][j - 1] = dp[i - 1][j - 1 - i + 1] + dp[i - 1][j - 1 - i + 1] + ... + dp[i - 1][j - 1];
-                //dp[i][j] - dp[i - 1][j - 1] = dp[i - 1][j] - dp[i - 1][j - i]
-                //由于j - 1 - i可能小于0，因此减的时候需要判断避免越界
-                dp[i][j] = (dp[i][j - 1] + dp[i - 1][j] - (j - i + 1 > 0 ? dp[i - 1][j - i] : 0)) % mod;
-                //由于取模后减出来可能是负数，需要特殊处理
-                if (dp[i][j] < 0) {
-                    dp[i][j] += mod;
-                }
+        int[][] dp = new int[2][k + 1];
+        dp[1][0] = 1;
+        for (int i = 2; i <= n; i++) {
+            int prev = (i - 1) % 2;
+            int curr = i % 2;
+            dp[curr][0] = 1;
+            for (int j = 1; j <= k; j++) {
+                
             }
         }
-        return dp[n][k];
-    }
-
-    /**
-     * n个数，最大逆序数对数量(1 + 2 + 3 + ... + n-1)
-     */
-    int maxPairs(int n) {
-        return n * (n - 1) / 2;
     }
 }
+
 
 //class Solution {
 //    private static final int MOD = (int) 1e9 + 7;
 //
 //    public int kInversePairs(int n, int k) {
+//        // 如果状态转移方程中，列数永远小于 则可以只用一个一维数组，此题不行
+//        int[][] dp = new int[2][k + 1];
+//        dp[1][0] = 1;
+//        for (int i = 2; i <= n; i++) {
+//            int prev = (i - 1) % 2;
+//            int curr = i % 2;
+//            dp[curr][0] = 1;
+//
+//            for (int j = 1; j <= k; j++) {
+//                dp[curr][j] = (dp[curr][j - 1] + dp[prev][j]) % MOD;
+//                if (j >= i) {
+//                    // 因为 a - b 中 a是取余后的数，所以差可能为负，不符合实际，需要+MOD修正一下
+//                    dp[curr][j] = (dp[curr][j] - dp[prev][j - i] + MOD) % MOD;
+//                }
+//            }
+//        }
+//
+//        return dp[n % 2][k];
+//    }
+//}
+
+
+/**
+ * 朴素实现 时间复杂度 O(N^2 * K) 空间复杂度 O(N * K)
+ */
+//class Solution {
+//    private static final int MOD = (int) 1e9 + 7;
+//
+//    public int kInversePairs(int n, int k) {
 //        int[][] dp = new int[n + 1][k + 1];
-//        int[][] preSum = new int[n + 1][k + 1];
-//        for (int i = 1; i <= n; i++) {
+//        dp[1][0] = 1;
+//        for (int i = 2; i <= n; i++) {
 //            for (int j = 0; j <= k; j++) {
 //                if (j == 0) {
 //                    dp[i][j] = 1;
-//                    preSum[i][j] = 1;
-//                    continue;
-//                }
-//                for (int p = 0; p <= Math.min(j, i - 1); p++) {
-//                    dp[i][j] = (dp[i][j] + dp[i - 1][j - p]) % MOD;
+//                } else {
+//                    for (int p = j; p >= Math.max(j - (i - 1), 0); p--) {
+//                        dp[i][j] = (dp[i][j] + dp[i - 1][p]) % MOD;
+//                    }
 //                }
 //            }
 //        }
